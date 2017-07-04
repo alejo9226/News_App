44de51b4463e1d8292c5a635343393516dd22ab8
alejo9226 <alejo9226@gmail.com>
2017-07-03 21:40:30 -0500

    Created the list item layout

--- app/src/main/java/alejandroalfaro/example/com/news_app/NewsActivity.java
+++ app/src/main/java/alejandroalfaro/example/com/news_app/NewsActivity.java
@@ -1,13 +1,28 @@
 package alejandroalfaro.example.com.news_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
+        setContentView(R.layout.list_item);
+    }
+
+    // Método para mostrar una imagen que viene de un link de internet
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
